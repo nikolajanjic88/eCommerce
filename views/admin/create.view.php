b8aa87907e710109c14ea13bef6de1150fcea494
Nikola Janjic <109689444+nikolajanjic88@users.noreply.github.com>
2023-04-06 19:09:19 +0200

    Add files via upload

--- views/admin/create.view.php
+++ views/admin/create.view.php
@@ -8,15 +8,15 @@
     <div class="row">
         <div class="edit">
             <form action="" method="post">
-                <input class="input-edit" type="text" name="title" value="<?= $_POST['title'] ?? null ?>">
+                <input class="input-edit" type="text" name="title" placeholder="Product Title">
                 <?php if(isset($errors['title'])): ?>
                 <p class="error"><?= $errors['title'] ?></p>
                 <?php endif ?>
-                <textarea class="input-edit" name="desc" id="" cols="30" rows="10"><?= $_POST['desc'] ?? null ?></textarea>
+                <textarea class="input-edit" name="desc" id="" cols="30" rows="10">Product description</textarea>
                 <?php if(isset($errors['desc'])): ?>
                 <p class="error"><?= $errors['desc'] ?></p>
                 <?php endif ?>
-                <input class="input-edit" type="number" step="0.01" name="price" value="<?= $_POST['price'] ?? null ?>">
+                <input class="input-edit" type="number" step="0.01" name="price" placeholder="Product Price">
                 <?php if(isset($errors['price'])): ?>
                 <p class="error"><?= $errors['price'] ?></p>
                 <?php endif ?>
